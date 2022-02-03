0325f1351ac2c9970afcf58679e7f9c74eb4a87a
Andrey Kravchenko <kravch2013@yandex.ru>
2022-02-03 21:07:36 +0700

    Bugfixes

--- qecontourgraph.cpp
+++ qecontourgraph.cpp
@@ -8,11 +8,8 @@ QEContourGraph::QEContourGraph(SettingsKeeper* settings, const QString& title, P
 
 void QEContourGraph::draw(QeSurfData* data)
 {
+    //data->rotateData(plotParams->tab5surfaceRotate * 90);
     drawSurface(data);
-
-    customPlot->setGeometry(30, 30, plotParams->drawRes, plotParams->drawRes);
-    customPlot->setMinimumSize(plotParams->drawRes, plotParams->drawRes);
-    customPlot->setMaximumSize(plotParams->drawRes, plotParams->drawRes);
 
     QVector<double> tickValues;
     QVector<QString> tickLabels;
@@ -23,6 +20,50 @@ void QEContourGraph::draw(QeSurfData* data)
     customPlot->xAxis->rescale(true);
     customPlot->yAxis->rescale(true);
 
+    const QCPRange xRange = customPlot->xAxis->range();
+    const QCPRange yRange = customPlot->yAxis->range();
+    compareScale = (yRange.upper - yRange.lower) / (xRange.upper - xRange.lower);
+    if (compareScale < 1.01)
+    {
+        customPlot->setGeometry(30, 30, plotParams->drawRes, static_cast<int>(round(plotParams->drawRes * compareScale)));
+        customPlot->setMinimumSize(plotParams->drawRes, static_cast<int>(round(plotParams->drawRes * compareScale)));
+        customPlot->setMaximumSize(plotParams->drawRes, static_cast<int>(round(plotParams->drawRes * compareScale)));
+    }
+    else
+    {
+        customPlot->setGeometry(30, 30, static_cast<int>(round(plotParams->drawRes / compareScale)), plotParams->drawRes);
+        customPlot->setMinimumSize(static_cast<int>(round(plotParams->drawRes / compareScale)), plotParams->drawRes);
+        customPlot->setMaximumSize(static_cast<int>(round(plotParams->drawRes / compareScale)), plotParams->drawRes);
+    }
+
+    if (plotParams->tab5surfaceHideAxis == true)
+    {
+        customPlot->xAxis->setTicks(false);
+        customPlot->xAxis->setTickLabels(false);
+        customPlot->yAxis->setTicks(false);
+        customPlot->yAxis->setTickLabels(false);
+    }
+    switch (plotParams->tab5surfaceRotate * 90)
+    {
+    case 0:
+        customPlot->xAxis->setRangeReversed(false);
+        customPlot->yAxis->setRangeReversed(false);
+        break;
+    case 90:
+        customPlot->xAxis->setRangeReversed(false);
+        customPlot->yAxis->setRangeReversed(true);
+        break;
+    case 180:
+        customPlot->xAxis->setRangeReversed(true);
+        customPlot->yAxis->setRangeReversed(true);
+        break;
+    case 270:
+        customPlot->xAxis->setRangeReversed(true);
+        customPlot->yAxis->setRangeReversed(false);
+        break;
+    default:
+        break;
+    }
     customPlot->show();
 }
 
@@ -32,7 +73,7 @@ void QEContourGraph::drawSurface(QeSurfData* surfData) const
 
     pen.setStyle(Qt::PenStyle::SolidLine);
     pen.setColor(QColor(0, 0, 0));
-    pen.setWidthF(plotParams->tab5surfaceWidth);
+    pen.setWidthF(plotParams->tab5surfaceWidth); 
 
     std::vector<std::vector<double>> test;
     for (int i = 0; i < surfData->oY.count(); i++)
